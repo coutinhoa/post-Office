a4608990475b4159c835479df917d94c326b8c99
A_coutinho <coutinhofalice@gmail.com>
2023-04-05 17:30:02 +0200

    Seed packages table

---
+++ src/main/resources/db/migration/V8__Add_packages.sql
@@ -0,0 +1,9 @@
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (1, 2.0, '10-04-2021');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (2, 2.5, '12-06-2022');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (3, 1.3, '09-01-2021');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (4, 2.1, '02-05-2023');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (5, 5.2, '20-03-2022');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (6, 4.2, '20-03-2022');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (7, 2.0, '09-01-2021');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (8, 3.9, '11-07-2022');
+insert into packages (ID, WEIGHT, DELIVERY_DATE) values (9, 0.7,'03-11-2022');
