46e9a9423209651ec402b2cf843bfe99013717d4
coutinhoa <104270514+coutinhoa@users.noreply.github.com>
2023-04-05 17:28:48 +0200

    [DHL4] Seed countries table (#4)

---
+++ src/main/resources/db/migration/V7__Add_countries.sql
@@ -0,0 +1,5 @@
+insert into countries (ID, NAME) values (1, 'De');
+insert into countries (ID, NAME) values (2, 'Pt');
+insert into countries (ID, NAME) values (3, 'Spain');
+insert into countries (ID, NAME) values (4,'France');
+insert into countries (ID, NAME) values (5,'Netherlands');
