77490bc3f1eda38be466acd8a10124b3fcd7ba97
coutinhoa <104270514+coutinhoa@users.noreply.github.com>
2023-04-05 17:26:24 +0200

    [DHL3] Seed companies table (#3)

---
+++ src/main/resources/db/migration/V6__Add_companies.sql
@@ -0,0 +1,4 @@
+insert into companies (ID, NAME) values (1, 'AA');
+insert into companies (ID, NAME) values (2, 'BB');
+insert into companies (ID, NAME) values (3, 'CC');
+insert into companies (ID, NAME) values (4,'DD');
