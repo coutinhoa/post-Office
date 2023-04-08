d5d14d2a36f57ee95442dd891b1abd1c3554deb7
coutinhoa <104270514+coutinhoa@users.noreply.github.com>
2023-04-08 22:01:41 +0200

    [DHL8] Seed users in packages (#8)

---
+++ src/main/resources/db/migration/V12__Add_sender_addressee.sql
@@ -0,0 +1,9 @@
+update packages set sender_id=3, addressee_id=2 where id=1;
+update packages set sender_id=1, addressee_id=12 where id=2;
+update packages set sender_id=4, addressee_id=15 where id=3;
+update packages set sender_id=13, addressee_id=2 where id=4;
+update packages set sender_id=7, addressee_id=6 where id=5;
+update packages set sender_id=3, addressee_id=10 where id=6;
+update packages set sender_id=8, addressee_id=9 where id=7;
+update packages set sender_id=11, addressee_id=5 where id=8;
+update packages set sender_id=2, addressee_id=7 where id=9;
